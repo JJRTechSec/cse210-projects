1cfcc2724ec9779b9f46d97c6f31fed9a572445c
JJRTechSec <jjrtechsec@gmail.com>
2025-07-16 21:33:17 +0200

    Created Fraction class, added all attributes, constructors and methods and ensured all works correctly

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -3,21 +3,42 @@ using System;
 public class Fraction
 {
     // ATTRIBUTES
-    private int top;
-    private int bottom;
+    private int _top;
+    private int _bottom;
 
     // CONSTRUCTORS
-    //private void Fraction()
-    //private void Fraction(int wholeNumber)
-    //private void Fraction(int top, int bottom)
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
 
     // GETTERS AND SETTERS
-    //GetTop()
-    //SetTop(int top)
-    //GetBottom()
-    //SetBottom(int bottom)
+    //public string GetTop()
+    //public int SetTop(int top)
+    //public void GetBottom()
+    //public int SetBottom(int bottom)
 
     // METHODS
-    //private string Fraction()
-    //private double Fraction()
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double) _top / _bottom;
+    }
 }
