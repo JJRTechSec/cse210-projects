193f9cec6bc6ab5e61f5e2b4c1b8b70ec3eb9369
JJRTechSec <jjrtechsec@gmail.com>
2025-07-16 20:54:24 +0200

    Added the class diagram to the files

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Fraction
+{
+    // ATTRIBUTES
+    private int top;
+    private int bottom;
+
+    // CONSTRUCTORS
+    //private void Fraction()
+    //private void Fraction(int wholeNumber)
+    //private void Fraction(int top, int bottom)
+
+    // GETTERS AND SETTERS
+    //GetTop()
+    //SetTop(int top)
+    //GetBottom()
+    //SetBottom(int bottom)
+
+    // METHODS
+    //private string Fraction()
+    //private double Fraction()
+}
