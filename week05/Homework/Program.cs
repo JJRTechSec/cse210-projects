4abd01d624bf728e3c9d90eccad8da7c21b8f571
JJRTechSec <jjrtechsec@gmail.com>
2025-07-30 11:47:23 +0200

    Created assignment, maths and writing assignment classes. Maths and assignment function as they should

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Joseph", "Exponentiation");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment maths = new MathAssignment("Joseph", "Exponentiation", "3.6", "1-9");
+        Console.WriteLine(maths.GetHomeworkList());
     }
 }
